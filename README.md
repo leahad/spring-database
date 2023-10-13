31c3a0280997347dcdffa5908a5f9206e3b6cdde
LEA HADIDA <hadida.lea@gmail.com>
2023-10-13 10:47:25 +0200

    added info on readme.md

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# Create a Database with String
+# Create a Database in Spring Boot applications
+
+### setup
+
+### preload data with SQL script
+
+1/Create an import.sql file in the src/main/resources directory.
+2/add the line `spring.jpa.hibernate.ddl-auto=create-drop` in application.properties
+
+### CommandLineRunner interface
+
+Add this code to your 
+
 
 ### Reference Documentation
 For further reference, please consider the following sections:
+
+https://www.masterspringboot.com/data-access/jpa-applications/preloading-data-in-spring-boot-with-import-sql-and-data-sql/
 
 * [Spring Documentation](https://docs.spring.io/spring-boot/docs/3.1.4/reference/htmlsingle/index.html#data.sql.jpa-and-spring-data)
 
--- src/main/java/com/wild/bdd/BddApplication.java
+++ src/main/java/com/wild/bdd/BddApplication.java
@@ -1,13 +1,38 @@
 package com.wild.bdd;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wild.bdd.entity.Quest;
+import com.wild.bdd.entity.Student;
+import com.wild.bdd.repository.QuestRepository;
+import com.wild.bdd.repository.StudentRepository;
 
 @SpringBootApplication
 public class BddApplication {
 
+	private QuestRepository questRepository;
+	private StudentRepository studentRepository;
+
+	public BddApplication(QuestRepository questRepository, StudentRepository studentRepository) {
+		this.questRepository = questRepository;
+		this.studentRepository = studentRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BddApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() throws Exception {
+		return (String[] args) -> {
+			Quest quest = new Quest("Code Wars", 20);
+			questRepository.save(quest);
+			Student student = new Student("Juliette");
+			studentRepository.save(student);
+		};
 	}
 
 }
